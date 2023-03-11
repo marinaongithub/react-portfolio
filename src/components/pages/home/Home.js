d3fbbb9b80cf0d5594e0ab4bcad50f011a5d0a25
marina.theotiste@gmail.com <73482347+marinaongithub@users.noreply.github.com>
2023-03-11 18:34:07 +0000

    Added Home component and styling

---
+++ src/components/pages/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import avatar from './profile.jpg'
+
+
+function Home() {
+  return (
+    <div className="jumbotron" id="home">
+        <img src={avatar} alt='avatar'/>
+        <h1 className="display-4">My Portfolio.</h1>
+        <p className="lead">Have a look around to discover my projects, learn a bit more about me and my skills.</p>
+        <p>Contact me if you like my work!</p>
+    </div>
+  )
+}
+
+export default Home
