26ba01b7ec4fee091592cee1e591427feafb80c5
marina.theotiste@gmail.com <73482347+marinaongithub@users.noreply.github.com>
2023-03-11 20:06:50 +0000

    Added header component + styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Home from './components/pages/home/Home';
+import Header from './components/Header';
 
 function App() {
   return (
-    
+    <div>
+      <Header/>
       <Home/>
-
+    </div>
   );
 }
 
