b69e49b62b425c1df0dd033dcc82a15f9e5f1b89
marinaongithub <73482347+marinaongithub@users.noreply.github.com>
2023-03-14 22:49:25 +0000

    Updated Homepage path

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Router basename="/react-portfolio">
         <Header/>
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/home" element={<Home/>}/>
           <Route path="/contact" element={<Contact/>}/>
           <Route path="/projects" element={<ProjectPage/>}/>
         </Routes>
